702cca4c8e15804b47fcc3bdff2243885249e661
libraiger <misssilverbeauty0927@outlook.com>
2021-06-18 15:35:24 +0300

    get video length in frames

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ path = "./dataset_raw/10 Minutes of HACKER USING Aimbot & Wall Hack in Apex Lege
 video = cv2.VideoCapture(path)
 
 fps = video.get(cv2.CAP_PROP_FPS)
+length = video.get(cv2.CAP_PROP_FRAME_COUNT)
 
-print ("Frames per second using video.get(cv2.CAP_PROP_FPS) : {0}".format(fps))
-
-if os.path.isdir(path):  
-    print("\nIt is a directory")  
-elif os.path.isfile(path):
-  file_type, other = mimetypes.guess_type(path)
-  print("File type is {0}".format(file_type))
+print("Frames per second using video.get(cv2.CAP_PROP_FPS) : {0}".format(fps))
+print("Video length in frames: {0}".format(length))
 
 video.release()
+
+# if os.path.isdir(path):  
+#     print("\nIt is a directory")  
+# elif os.path.isfile(path):
+#   file_type, other = mimetypes.guess_type(path)
+#   print("File type is {0}".format(file_type))
