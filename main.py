ad9eb025c08e2a302522128251483bd9fd1bb001
libraiger <misssilverbeauty0927@outlook.com>
2021-06-18 15:40:35 +0300

    get video length in mins, secs and ms

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 import mimetypes
 import os
+import math
 
 path = "./dataset_raw/10 Minutes of HACKER USING Aimbot & Wall Hack in Apex Legends Season 9!.mp4"
 video = cv2.VideoCapture(path)
@@ -10,6 +11,10 @@ length = video.get(cv2.CAP_PROP_FRAME_COUNT)
 
 print("Frames per second using video.get(cv2.CAP_PROP_FPS) : {0}".format(fps))
 print("Video length in frames: {0}".format(length))
+ms = length % fps
+secs = math.floor(length / fps)
+mins = math.floor(secs / 60)
+print("Video length: {0} mins {1} sec {2} ms".format(mins, secs % 60, ms))
 
 video.release()
 
