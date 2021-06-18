c7759d543703b9e5b6f4e61baf88b85ac5ef7259
libraiger <misssilverbeauty0927@outlook.com>
2021-06-18 15:29:19 +0300

    check if path is dir or file

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import cv2
 import mimetypes
+import os
 
-video = cv2.VideoCapture("./dataset_raw/10 Minutes of HACKER USING Aimbot & Wall Hack in Apex Legends Season 9!.mp4")
+path = "./dataset_raw/10 Minutes of HACKER USING Aimbot & Wall Hack in Apex Legends Season 9!.mp4"
+video = cv2.VideoCapture(path)
 
 fps = video.get(cv2.CAP_PROP_FPS)
 
-file_type, other = mimetypes.guess_type("./dataset_raw/10 Minutes of HACKER USING Aimbot & Wall Hack in Apex Legends Season 9!.mp4")
-
 print ("Frames per second using video.get(cv2.CAP_PROP_FPS) : {0}".format(fps))
-print("File type is {0}".format(file_type))
+
+if os.path.isdir(path):  
+    print("\nIt is a directory")  
+elif os.path.isfile(path):
+  file_type, other = mimetypes.guess_type(path)
+  print("File type is {0}".format(file_type))
 
 video.release()
