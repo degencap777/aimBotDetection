5c8b82124e6385079b4cfc3a0e506f632a81cdff
libraiger <misssilverbeauty0927@outlook.com>
2021-06-20 11:10:52 +0300

    downsample frames

--- process_input.py
+++ process_input.py
@@ -7,9 +7,6 @@ import random
 
 # video = cv2.VideoCapture(path)
 
-# fps = video.get(cv2.CAP_PROP_FPS)
-# length = video.get(cv2.CAP_PROP_FRAME_COUNT)
-
 # print("Frames per second using video.get(cv2.CAP_PROP_FPS) : {0}".format(fps))
 # print("Video length in frames: {0}".format(length))
 # ms = length % fps
@@ -17,45 +14,69 @@ import random
 # mins = math.floor(secs / 60)
 # print("Video length: {0} mins {1} sec {2} ms".format(mins, secs % 60, ms))
 
-# Define the codec and create VideoWriter object
-# fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-# specify last param for greyscale
-# out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (640,  480), 0)
-
 
 # input dir path
-input_dataset_path = "./dataset/"
-# output dir path
-output_path = "./dataset_processed"
-# traverse the input dir
-cheater_clips = os.listdir(input_dataset_path + "cheating")
-not_cheater_clips = os.listdir(input_dataset_path + "not_cheating")
-# randomly select test data
-cheater_percentage = len(cheater_clips) * 20 / 100
-test_cheater_clips = random.sample(cheater_clips, math.floor(cheater_percentage))
-not_cheater_percentage = len(not_cheater_clips) * 20 / 100
-test_not_cheater_clips = random.sample(not_cheater_clips, math.floor(not_cheater_percentage))
-# create output dir or throw error if already exists
-if os.path.exists(output_path) is False:
-  os.mkdir(output_path)
-if len(os.listdir(output_path)) > 0:
-  raise FileExistsError("Provide a clean output directory")
+# input_dataset_path = "./dataset/"
+# # output dir path
+# output_path = "./dataset_processed"
+# # traverse the input dir
+# cheater_clips = os.listdir(input_dataset_path + "cheating")
+# not_cheater_clips = os.listdir(input_dataset_path + "not_cheating")
+# # randomly select test data
+# cheater_percentage = len(cheater_clips) * 20 / 100
+# test_cheater_clips = random.sample(cheater_clips, math.floor(cheater_percentage))
+# not_cheater_percentage = len(not_cheater_clips) * 20 / 100
+# test_not_cheater_clips = random.sample(not_cheater_clips, math.floor(not_cheater_percentage))
+# # create output dir or throw error if already exists
+# if os.path.exists(output_path) is False:
+#   os.mkdir(output_path)
+# if len(os.listdir(output_path)) > 0:
+#   raise FileExistsError("Provide a clean output directory")
 
-os.mkdir(output_path + "/test")
-for clip in test_cheater_clips:
-  # print(clip)
-  shutil.copyfile("{0}cheating/{1}".format(input_dataset_path, clip), output_path + "/test/" + clip)
+# os.mkdir(output_path + "/test")
+# for clip in test_cheater_clips:
+#   shutil.copyfile("{0}cheating/{1}".format(input_dataset_path, clip), output_path + "/test/" + clip)
 
-for clip in test_not_cheater_clips:
-  shutil.copyfile("{0}not_cheating/very-good-players/{1}".format(input_dataset_path, clip), output_path + "/test/" + clip)
+# for clip in test_not_cheater_clips:
+#   shutil.copyfile("{0}not_cheating/very-good-players/{1}".format(input_dataset_path, clip), output_path + "/test/" + clip)
 
-print("Create test dataset with {0} clips with cheaters and {1} clips with players. Total: {2}"
-      .format(
-        len(test_cheater_clips),
-        len(test_not_cheater_clips),
-        len(test_cheater_clips) + len(test_not_cheater_clips)
-      ))
+# print("Create test dataset with {0} clips with cheaters and {1} clips with players. Total: {2}"
+#       .format(
+#         len(test_cheater_clips),
+#         len(test_not_cheater_clips),
+#         len(test_cheater_clips) + len(test_not_cheater_clips)
+#       ))
+
 # remove frames to get an fps of 20
+def downsample_frames(clip_name: str, target_fps = 20):
+  clip = cv2.VideoCapture(clip_name)
+  fps = clip.get(cv2.CAP_PROP_FPS)
+  length = clip.get(cv2.CAP_PROP_FRAME_COUNT)
+
+  # Define the codec and create VideoWriter object
+  fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+  # specify last param for greyscale
+  out = cv2.VideoWriter('output.mp4', fourcc, target_fps, (640,  480), 0)
+
+  frame_count = 0
+  while clip.isOpened():
+    ret, frame = clip.read()
+    if not ret:
+      print("Can't receive frame (stream end?). Exiting ...")
+      break
+
+    if length < fps:
+      out.write(frame)
+    else:
+      if fps == 30 and frame_count % 3 == 2:
+        out.write(frame)
+      if fps == 60 and frame_count % 2 == 1:
+        out.write(frame)
+    frame_count += 1
+
+  clip.release()
+  out.release()
+
 # convert frames to grayscale
 # context stream: downsize frames to 89 x 89
 # output file: name-context.mp4
