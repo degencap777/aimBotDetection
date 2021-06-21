eb0801a314c8ef3f83b22eb7f12b01d3a800a1e4
libraiger <misssilverbeauty0927@outlook.com>
2021-06-21 10:54:41 +0300

    get delay from input and display video info

--- video-reader.py
+++ video-reader.py
@@ -3,11 +3,15 @@ import cv2
 
 if __name__ == "__main__":
   video_name = sys.argv[1]
+  delay = sys.argv[2] if sys.argv[2] else 500
   if not video_name:
     raise ValueError("Please provide a video path as first arg!")
 
   clip = cv2.VideoCapture(video_name)
   fps = clip.get(cv2.CAP_PROP_FPS)
+  length = clip.get(cv2.CAP_PROP_FRAME_COUNT)
+
+  print("Total frames: {0}, fps: {1}".format(length, fps))
 
   while clip.isOpened():
     ret, frame = clip.read()
@@ -16,7 +20,7 @@ if __name__ == "__main__":
       break
     
     cv2.imshow("clip_name", frame)
-    if cv2.waitKey(1000) == ord('q'):
+    if cv2.waitKey(delay) == ord('q'):
       break
 
   clip.release()
