036556ebc63d1282e242c0bcfda1b016b046f032
libraiger <misssilverbeauty0927@outlook.com>
2021-06-23 21:58:10 +0300

    compile model

--- multi-res-cnn.py
+++ multi-res-cnn.py
@@ -5,8 +5,10 @@ import argparse
 import os
 import cv2
 from tensorflow import keras
-from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input
 from tensorflow.keras.layers import Dense, Flatten, Conv3D, MaxPooling3D, Dropout
+from tensorflow.keras.layers import concatenate
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.utils import plot_model
 import numpy as np
@@ -68,8 +70,8 @@ if not args.test:
 
     labels_train.append(label)
     X_train.append(video_to_array(file_path))
-    labels_train.append(label)
-    X_train.append(video_to_array(file_path, flip=True))
+    # labels_train.append(label)
+    # X_train.append(video_to_array(file_path, flip=True))
 
   progress_bar.close()
 
@@ -108,22 +110,27 @@ print('X_shape:{}\nY_shape:{}'.format(X_test.shape, Y_test.shape))
 input_shape = X_train.shape[1:] if len(X_train) else X_test.shape[1:]
 
 # Create the model
-model = Sequential()
-model.add(Conv3D(32, kernel_size=(3, 3, 3), activation='relu', kernel_initializer='he_uniform',
-  input_shape=(input_shape), padding="same"))
-model.add(Conv3D(32, kernel_size=(3, 3, 3), activation='softmax', padding="same"))
-model.add(MaxPooling3D(pool_size=(3, 3, 3), padding="same"))
-model.add(Dropout(0.25))
-
-model.add(Conv3D(64, kernel_size=(3, 3, 3), activation='relu', padding="same"))
-model.add(Conv3D(64, kernel_size=(3, 3, 3), activation='softmax', padding="same"))
-model.add(MaxPooling3D(pool_size=(3, 3, 3), padding="same"))
-model.add(Dropout(0.25))
-
-model.add(Flatten())
-model.add(Dense(512, activation='sigmoid'))
-model.add(Dropout(0.5))
-model.add(Dense(no_classes, activation='softmax'))
+# input context stream
+visible1 = Input(shape=input_shape)
+conv11 = Conv3D(32, kernel_size=(3, 3, 3), activation='relu', kernel_initializer='he_uniform')(visible1)
+pool11 = MaxPooling3D(pool_size=(2, 2, 2), padding="same")(conv11)
+conv12 = Conv3D(16, kernel_size=(3, 3, 3), activation='relu', padding="same")(pool11)
+pool12 = MaxPooling3D(pool_size=(2, 2, 2), padding="same")(conv12)
+flat1 = Flatten()(pool12)
+# input fovea stream
+visible2 = Input(shape=input_shape)
+conv21 = Conv3D(32, kernel_size=(3, 3, 3), activation='relu', kernel_initializer='he_uniform')(visible2)
+pool21 = MaxPooling3D(pool_size=(2, 2, 2), padding="same")(conv21)
+conv22 = Conv3D(16, kernel_size=(3, 3, 3), activation='relu', padding="same")(pool21)
+pool22 = MaxPooling3D(pool_size=(2, 2, 2), padding="same")(conv22)
+flat2 = Flatten()(pool22)
+# merge input models
+merge = concatenate([flat1, flat2])
+# interpretation model
+hidden1 = Dense(10, activation='relu')(merge)
+hidden2 = Dense(10, activation='relu')(hidden1)
+output = Dense(no_classes, activation='softmax')(hidden2)
+model = Model(inputs=[visible1, visible2], outputs=output)
 
 # Compile the model
 model.compile(loss=keras.losses.categorical_crossentropy,
@@ -134,34 +141,34 @@ plot_model(model, show_shapes=True,
             to_file=os.path.join('multi-res-cnn-model.png'))
 
 # Load weights if provided
-if args.load is not False:
-  model.load_weights(args.load)
-# Fit data to model
-if not args.test:
-  history = model.fit(X_train, Y_train,
-              validation_data=(X_test, Y_test),
-              batch_size=batch_size,
-              epochs=no_epochs,
-              verbose=verbosity,
-              shuffle=True)
+# if args.load is not False:
+#   model.load_weights(args.load)
+# # Fit data to model
+# if not args.test:
+#   history = model.fit(X_train, Y_train,
+#               validation_data=(X_test, Y_test),
+#               batch_size=batch_size,
+#               epochs=no_epochs,
+#               verbose=verbosity,
+#               shuffle=True)
 
-# # Generate generalization metrics
-loss, acc = model.evaluate(X_test, Y_test, verbose=0)
-print('Test loss:', loss)
-print('Test accuracy:', acc)
+# # # Generate generalization metrics
+# loss, acc = model.evaluate(X_test, Y_test, verbose=0)
+# print('Test loss:', loss)
+# print('Test accuracy:', acc)
 
-if not os.path.isdir(args.output):
-  os.mkdir(args.output)
-if not args.test:
-  model.save_weights(os.path.join(args.output, "multi-res-cnn-{0}-{1}-acc-{2}.h5".format(batch_size, no_epochs, round(acc, 2))))
+# if not os.path.isdir(args.output):
+#   os.mkdir(args.output)
+# if not args.test:
+#   model.save_weights(os.path.join(args.output, "multi-res-cnn-{0}-{1}-acc-{2}.h5".format(batch_size, no_epochs, round(acc, 2))))
 
-  # # Plot history: Categorical crossentropy & Accuracy
-  plt.plot(history.history['loss'], label='Categorical crossentropy (training data)')
-  plt.plot(history.history['val_loss'], label='Categorical crossentropy (validation data)')
-  plt.plot(history.history['accuracy'], label='Accuracy (training data)')
-  plt.plot(history.history['val_accuracy'], label='Accuracy (validation data)')
-  plt.title('Model performance for Conv3D for aimbot detection')
-  plt.ylabel('Loss value')
-  plt.xlabel('No. epoch')
-  plt.legend(loc="upper left")
-  plt.show()
+#   # # Plot history: Categorical crossentropy & Accuracy
+#   plt.plot(history.history['loss'], label='Categorical crossentropy (training data)')
+#   plt.plot(history.history['val_loss'], label='Categorical crossentropy (validation data)')
+#   plt.plot(history.history['accuracy'], label='Accuracy (training data)')
+#   plt.plot(history.history['val_accuracy'], label='Accuracy (validation data)')
+#   plt.title('Model performance for Conv3D for aimbot detection')
+#   plt.ylabel('Loss value')
+#   plt.xlabel('No. epoch')
+#   plt.legend(loc="upper left")
+#   plt.show()
